2b71c1e579991bf5dfb312022db9013247c38f4d
PawelPavo <Pawelebllc@gmail.com>
2020-01-22 19:49:53 -0500

    woring version - needs correct function order

--- script.js
+++ script.js
@@ -8,23 +8,20 @@ var weapons = ['Nuke', 'Fork', 'Toothpick', 'Shard of glass', 'Sponge', 'Towel',
 for (var i = 1; i < 101; i++) {
     var h3 = $('<h3 class="accused">Accusation ' + i + '</h3>');
     $('body').append(h3);
-    
-}
-
-
-//creating a click function for h3's (works but not comepletly ...does not compute ACCUSSATION)
-// This is what I am stuck on, how to connect the h3 with the alert correctly.
-$('h3').click(function () {
-    (num())
-});
-
-
-//created a counter for each click along with the alert
-var num = counter();
-function counter() {
-    i = 0;
-    return function() {
-        i++;
-        alert('Accusation ' + i + ': I accuse ' + friends[i % friends.length] + ', with the ' + weapons[i % weapons.length] + ' in the ' + locations[i % locations.length] + '!')
-    }
+    //appending click listiner
+    $(h3).click(function (e) {
+        //getting h3 text content for the alert box
+        var x = e.target.textContent
+        var num = accusation();
+        //creating the accusation text for the alert box
+        function accusation() {
+            return function () {
+                alert(x + ': I accuse ' + friends[i % friends.length] + ', with the ' + weapons[i % weapons.length] + ' in the ' + locations[i % locations.length] + '!')
+            }
+        }
+        //incrementing i for the alert box
+        i++
+        //invoking accusation function
+        num();
+    });
 }
